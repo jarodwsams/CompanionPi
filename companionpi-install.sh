17fb184492fbb20692e6ed852b6983c5d22e15d1
Jarod Sams <jarodsams@outlook.com>
2020-10-31 19:58:02 -0500

    adding symlink for companion-update command

--- companionpi-install.sh
+++ companionpi-install.sh
@@ -3,13 +3,13 @@
 ECHO "Installing dependencies"
 apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove
 apt install libgusb-dev npm nodejs git build-essential cmake libudev-dev libusb-1.0-0-dev -y
-ECHO "Dependencies installed"
+read -p "Dependencies installed" -t 5
 
 ECHO "Installing Node.js and Yarn"
 npm install n yarn -g
 n 8.12.0
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-ECHO "Node.js and Yarn installation complete"
+read -p "Node.js and Yarn installation complete" -t 5
 
 ECHO "Installing Companion"
 cd /usr/local/src
@@ -18,7 +18,7 @@ cd companion
 git checkout v2.1.0
 yarn update
 ./tools/build_writefile.sh
-ECHO "Companion installation complete"
+read -p "Companion installation complete" -t 5
 
 ECHO "Pulling updated supplemental CompanionPi repository"
 cd /usr/local/src
@@ -43,6 +43,9 @@ else
     cp systemd-service/companion.service /etc/systemd/system/companion.service
 fi
 read -p "Finishing up..." -t 5
+
+ln -s /usr/local/src/CompanionPi/companionpi-update.sh /usr/local/bin/companion-update
+chmod +x /usr/local/bin/companion-update
 
 read -p "Rebooting in 5 seconds..." -t 5
 reboot
